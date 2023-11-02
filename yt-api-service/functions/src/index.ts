b79dedf6b69bb4e501f80f03bd6691cbd10fb417
Christopher Lewis <chris.stephen.lewis@gmail.com>
2023-11-02 17:55:33 -0400

    Created user create firebase function

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -1,7 +1,21 @@
-import { onRequest } from 'firebase-functions/v2/https';
+import * as functions from 'firebase-functions';
+import { initializeApp } from 'firebase-admin/app';
+import { Firestore } from 'firebase-admin/firestore';
 import * as logger from 'firebase-functions/logger';
 
-export const helloWorld = onRequest((request, response) => {
-    logger.info('Hello logs!', { structuredData: true });
-    response.send('Hello from Firebase!');
+initializeApp();
+
+const firestore = new Firestore();
+
+export const createUser = functions.auth.user().onCreate((user) => {
+    const userInfo = {
+        uid: user.uid,
+        email: user.email,
+        photoUrl: user.photoURL,
+    };
+
+    // write to the users collection, doc specified by user id, and write the userInfo
+    firestore.collection('users').doc(user.uid).set(userInfo);
+    logger.info(`User Created: ${JSON.stringify(userInfo)}`);
+    return;
 });
