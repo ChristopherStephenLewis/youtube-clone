bdcdf3d029ab8ded8b4559444c39ee4ff8fef0d4
Christopher Lewis <chris.stephen.lewis@gmail.com>
2023-11-06 14:48:28 -0500

    Adding videos to firestore

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,58 +1,89 @@
 // https://cloud.google.com/run/docs/tutorials/pubsub#run_pubsub_server-nodejs
 // https://cloud.google.com/storage/docs/reference/libraries#client-libraries-usage-nodejs
 
-import express from 'express';
+import express from "express";
 import {
-    convertVideo,
-    deleteProcessedVideo,
-    deleteRawVideo,
-    downloadRawVideo,
-    setupDirectories,
-    uploadProcessedVideo,
-} from './storage';
+  convertVideo,
+  deleteProcessedVideo,
+  deleteRawVideo,
+  downloadRawVideo,
+  setupDirectories,
+  uploadProcessedVideo,
+} from "./storage";
+import { isVideoNew, setVideo } from "./firestore";
 
 setupDirectories();
 
 const app = express();
 app.use(express.json());
 
-app.post('/process-video', async (req, res) => {
-    // Get the bucket and filename from the Cloud Pub/Sub message
-    let data;
-    try {
-        const message = Buffer.from(req.body.message.data, 'base64').toString('utf8');
-        data = JSON.parse(message);
-        if (!data.name) {
-            throw new Error('Invalid message payload received.');
-        }
-    } catch (error) {
-        console.log(error);
-        return res.status(400).send('Bad Request: missing filename');
+app.post("/process-video", async (req, res) => {
+  // Get the bucket and filename from the Cloud Pub/Sub message
+  let data;
+  try {
+    const message = Buffer.from(req.body.message.data, "base64").toString(
+      "utf8"
+    );
+    data = JSON.parse(message);
+    if (!data.name) {
+      throw new Error("Invalid message payload received.");
     }
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send("Bad Request: missing filename");
+  }
 
-    const inputFileName = data.name;
-    const outputFileName = `processed-${inputFileName}`;
+  const inputFileName = data.name; // Format of <UID>-<DATE>.<EXTENSION>
+  const outputFileName = `processed-${inputFileName}`;
+  const videoId = inputFileName.split(".")[0];
 
-    // Download the raw video from Cloud Storage
-    await downloadRawVideo(inputFileName);
+  //Check if video is new
+  if (!isVideoNew(videoId)) {
+    return res
+      .status(400)
+      .send("Bad Request: video already processing or processed");
+  } else {
+    await setVideo(videoId, {
+      id: videoId,
+      uid: videoId.split("-")[0],
+      status: "processing",
+    });
+  }
 
-    //Convert the video to 360p
-    try {
-        await convertVideo(inputFileName, outputFileName);
-    } catch (err) {
-        await Promise.all([deleteRawVideo(inputFileName), deleteProcessedVideo(outputFileName)]);
-        console.log(err);
-        return res.status(500).send('Internal Server Error: video processing failed');
-    }
+  // Download the raw video from Cloud Storage
+  await downloadRawVideo(inputFileName);
 
-    //Upload the processed video to Cloud Storage
-    await uploadProcessedVideo(outputFileName);
-    await Promise.all([deleteRawVideo(inputFileName), deleteProcessedVideo(outputFileName)]);
+  //Convert the video to 360p
+  try {
+    await convertVideo(inputFileName, outputFileName);
+  } catch (err) {
+    await Promise.all([
+      deleteRawVideo(inputFileName),
+      deleteProcessedVideo(outputFileName),
+    ]);
+    console.log(err);
+    return res
+      .status(500)
+      .send("Internal Server Error: video processing failed");
+  }
 
-    return res.status(200).send('Processing finished successfully');
+  //Upload the processed video to Cloud Storage
+  await uploadProcessedVideo(outputFileName);
+
+  setVideo(videoId, {
+    status: "processed",
+    filename: outputFileName,
+  });
+
+  await Promise.all([
+    deleteRawVideo(inputFileName),
+    deleteProcessedVideo(outputFileName),
+  ]);
+
+  return res.status(200).send("Processing finished successfully");
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Video processing service listening at port ${port}`);
+  console.log(`Video processing service listening at port ${port}`);
 });
